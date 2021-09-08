be235e74b05ac0affeb07dc79a62f605f7406ad4
abhyudaya726 <71880557+abhyudaya726@users.noreply.github.com>
2021-09-08 10:41:28 +0530

    Update z-test.py

--- z-test.py
+++ z-test.py
@@ -23,15 +23,17 @@ meanList = []
 for i in range(0,1000):
     set_of_means = calc_rand_mean(100)
     meanList.append(set_of_means)
-mean = st.mean(meanList)
-print("Mean is:",mean)
+mean1 = st.mean(meanList)
+print("Mean is:",mean1)
 
-d_1_start,d_1_end = mean-stdev/10,mean+stdev/10
-d_2_start,d_2_end = mean-(stdev/10)*2,mean+(stdev/10)*2
-d_3_start,d_3_end = mean-(stdev/10)*3,mean+(stdev/10)*3
+d_1_start,d_1_end = mean1-stdev/10,mean+stdev/10
+d_2_start,d_2_end = mean1-(stdev/10)*2,mean+(stdev/10)*2
+d_3_start,d_3_end = mean1-(stdev/10)*3,mean+(stdev/10)*3
+
+zTest = (mean-mean1)/stdev1
 
 fig = ff.create_distplot([meanList],["reading_time"],show_hist=False)
-fig.add_trace(go.Scatter(x=[mean,mean],y=[0,1],mode="lines",name="mean"))
+fig.add_trace(go.Scatter(x=[mean1,mean1],y=[0,1],mode="lines",name="mean"))
 fig.add_trace(go.Scatter(x=[d_1_start,d_1_start],y=[0,1.2],mode="lines",name="D1 start"))
 fig.add_trace(go.Scatter(x=[d_1_end,d_1_end],y=[0,1.2],mode="lines",name="D1 end"))
 fig.add_trace(go.Scatter(x=[d_2_start,d_2_start],y=[0,1.2],mode="lines",name="D2 start"))
